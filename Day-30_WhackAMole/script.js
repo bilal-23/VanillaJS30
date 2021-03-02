b1c2cc1c426db9b47f5fc23933234487d74d41e5
Bilal Mohammad <mansuribilal101@gmail.com>
2021-03-02 15:07:44 +0530

    added game

---
+++ Day-30_WhackAMole/script.js
@@ -0,0 +1,55 @@
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+const scoreBoard = document.querySelector('.score');
+let lastHole; 
+let timeUp = false;
+let score;
+
+
+function randomTime(min,max){  //in milliseconds
+    return Math.random() * (max-min)+min ;
+}
+
+
+function randomHole(holes){
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+
+    if(hole === lastHole){
+        // console.log("same 1");
+        return randomHole(holes);
+    }  
+
+    lastHole = hole;
+    return hole;
+}
+
+function peep(){
+    const time = randomTime(200,1000);
+    const hole = randomHole(holes);
+    // console.log(time,hole);
+    hole.classList.add('up');
+    setTimeout(()=>{
+        hole.classList.remove('up');
+        if(!timeUp) peep();
+    }, time);
+}
+
+function startGame(){
+    scoreBoard.textContent = 0 ;
+    score = 0
+    timeUp = false;
+    peep();
+    setTimeout(()=> timeUp = true, 10000)
+}
+
+function bonk(e){
+if(!e.isTrusted) return ; //cheater
+score++;    
+this.classList.remove('up');
+console.log('score:', score)
+scoreBoard.textContent = score;
+}
+
+
+moles.forEach(mole => mole.addEventListener('click', bonk));
